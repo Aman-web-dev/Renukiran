8abf9a33de2558dfacdb54fd6e8f5d963a396b6e
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-09-21 13:44:10 +0530

    my changes

---
+++ app/help/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function help() {
+  return (
+    <div>
+      Hi this is Your Help
+    </div>
+  )
+}
+
+export default help
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+
+const nextConfig = {
+    experimental:{
+        appdir:true,
+    },
+    images: {
+        hostname:["images.freepik.com","images.pexels.com"],
+        domains:["images.freepik.com","images.pexels.com"]
+}
+}
+
+module.exports =nextConfig;
+
+
+
+
