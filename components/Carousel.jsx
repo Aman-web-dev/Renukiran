380d60c7e4518718bde522a49a75978eba58c952
Rajnikantbhay <rsusishere@gmail.com>
2023-09-22 17:12:00 +0530

    changes

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Carousel = () => {
   return (
     
-<div id="default-carousel" className="relative w-full mb-40" data-carousel="slide">
+<div id="default-carousel" className="relative w-full mb-40" data-carousel="static">
 
 
 
@@ -11,21 +11,24 @@ const Carousel = () => {
     <div className="relative overflow-hidden md:h-96" style={{minHeight:'90vh'}}>
     
         <div className="hidden ease-in-out duration-1000" data-carousel-item>
+            <div className='text-white text-6xl absolute font-bold z-10 '>
+                Donate for a child
+            </div>
             <img src="https://images.unsplash.com/photo-1542810634-71277d95dcbb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className=" grayscale-[100%] absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 " alt="..." />
         </div>
   
-        <div className="hidden duration-1000 ease-in-out" data-carousel-item>
+        <div className="hidden ease-in-out duration-1000 " data-carousel-item>
             <img src="https://images.unsplash.com/flagged/photo-1574097656146-0b43b7660cb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80
 
 " className="grayscale-[100%] absolute block h-full object-cover w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
       
-        <div className="hidden duration-1000 ease-in-out" data-carousel-item>
+        <div className="hidden ease-in-out duration-1000 " data-carousel-item>
             <img src="https://images.unsplash.com/photo-1496813146940-1601b02f81a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1449&q=80
 " className="grayscale-[100%] absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
       
-        <div className="hidden duration-1000 ease-in-out" data-carousel-item>
+        <div className="hidden ease-in-out duration-1000 " data-carousel-item>
             <img src="https://images.unsplash.com/photo-1441307811206-a12c74889338?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="grayscale-[100%] absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
     </div>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -17,16 +17,16 @@ const Nav = () => {
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-    <div className="hidden w-full md:block md:w-auto backdrop-filter backdrop-blur-lg bg-opacity-30" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 backdrop-filter backdrop-blur-lg bg-opacity-30">
+    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  md:flex-row md:space-x-8 md:mt-0 md:border-0">
         <li>
           <a href="" className="block py-2 pl-3 pr-4 text-gray-900 rounded md:bg-transparent md:p-0 md:hover:text-blue-700 md:dark:text-blue-500" aria-current="page">Our Work</a>
         </li>
         <li>
-          <a href="/help" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Donate us</a>
+          <a href="/help" className="block py-2 pl-3 pr-4 text-gray-900 rounded  md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Donate us</a>
         </li>
         <li>
-          <a href="/contact" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <a href="/contact" className="block py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
         <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
