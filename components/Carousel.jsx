1ab7ab1da3c99f1b37c4c9af6dd31fd4ae987b07
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-09-22 17:12:44 +0530

    Chnages hai ji

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -11,13 +11,17 @@ const Carousel = () => {
     <div className="relative overflow-hidden md:h-96" style={{minHeight:'90vh'}}>
     
         <div className="hidden ease-in-out duration-1000" data-carousel-item>
+{/* <p className='text-3xl absolute z-10'>hello sir</p>  */}
+
             <img src="https://images.unsplash.com/photo-1542810634-71277d95dcbb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className=" grayscale-[100%] absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 " alt="..." />
         </div>
   
         <div className="hidden duration-1000 ease-in-out" data-carousel-item>
+
             <img src="https://images.unsplash.com/flagged/photo-1574097656146-0b43b7660cb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80
 
 " className="grayscale-[100%] absolute block h-full object-cover w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+
         </div>
       
         <div className="hidden duration-1000 ease-in-out" data-carousel-item>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -11,14 +11,14 @@ const Nav = () => {
     <a href="/" className="flex items-center">
         <Image src={logo} width={90} height={10} alt="Flowbite Logo" />
     </a>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
     <div className="hidden w-full md:block md:w-auto backdrop-filter backdrop-blur-lg bg-opacity-30" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 backdrop-filter backdrop-blur-lg bg-opacity-30">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 backdrop-filter backdrop-blur-lg bg-opacity-30">
         <li>
           <a href="" className="block py-2 pl-3 pr-4 text-gray-900 rounded md:bg-transparent md:p-0 md:hover:text-blue-700 md:dark:text-blue-500" aria-current="page">Our Work</a>
         </li>
