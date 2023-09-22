eb650d1e4f7623b6d106e747fde4fa4e396b3a67
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-09-22 11:46:14 +0530

    Changes as On 22 Sep

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
-import Footer from '@/components/Footer'
-import Nav from '@/components/Nav'
 
 export default function Home() {
   return (
     <>
-    <Nav />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hello
     </main>
-    <Footer />
     </>
   )
 }
