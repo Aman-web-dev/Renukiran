844df8e33fcce634282ef5544d1dda209d49e439
Rajnikantbhay <rsusishere@gmail.com>
2023-09-22 11:45:19 +0530

    homepage edited

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import Footer from '@/components/Footer'
 import Nav from '@/components/Nav'
+import Carousel from '@/components/Carousel'
+import {register} from 'swiper/element/bundle';
+register();
+import Partners from '@/components/Partners';
 
 export default function Home() {
   return (
     <>
     <Nav />
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Carousel/>
+      <Partners />
     </main>
     <Footer />
     </>
