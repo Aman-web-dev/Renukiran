11becee52b84f43f9fc65a34cfa8776ae2013e73
Rajnikantbhay <rsusishere@gmail.com>
2023-09-29 10:16:47 +0530

    29 sept

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -11,8 +11,8 @@ const Carousel = () => {
     <div className="relative overflow-hidden md:h-96" style={{minHeight:'90vh'}}>
     
         <div className="hidden ease-in-out duration-1000" data-carousel-item>
-            <div className='text-white text-6xl absolute font-bold z-10 '>
-                Donate for a child
+            <div className='text-white text-6xl relative font-bold z-10 w-1/2 bg-gray-600 h-full flex items-center opacity-40'>
+                <span className='p-8 absolute opacity-100'>Donate for a child</span>
             </div>
             <img src="https://images.unsplash.com/photo-1542810634-71277d95dcbb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className=" grayscale-[100%] absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 " alt="..." />
         </div>
