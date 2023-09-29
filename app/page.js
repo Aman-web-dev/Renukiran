2b50aabe6ca6e4df939b5de4f35d4e2548d774f3
Rajnikantbhay <rsusishere@gmail.com>
2023-09-29 14:31:59 +0530

    rajni changes

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import {register} from 'swiper/element/bundle';
 register();
 import Partners from '@/components/Partners';
 import Analytics from '@/components/analytics';
+import Homeabout from '@/components/Homeabout';
+import AvinashInfo from '@/components/AvinashInfo';
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
 
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Carousel/>
+      <Homeabout />
+      <AvinashInfo />
       <Partners />
       <Analytics/>
     </main>
--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './component.css'
+import Button from '@mui/material/Button';
+
 
 const Carousel = () => {
   return (
     
-<div id="default-carousel" className="relative w-full mb-40" data-carousel="static">
+<div id="default-carousel" className="relative w-full mb-24" data-carousel="static">
 
 
 
@@ -14,6 +16,7 @@ const Carousel = () => {
         <div className="hidden ease-in-out duration-1000" data-carousel-item>
             <div className='md:text-white md:z-10 md:bg-[#2e5893]/91 hero-section'>
                 <h1 className='md:absolute'>Donate <br/>for a <br/>child</h1>
+                <Button className='donation-button' variant="contained">Donate now</Button>
             </div>
             <img src="https://images.unsplash.com/photo-1542810634-71277d95dcbb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className=" grayscale-[100%] absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 " alt="..." />
         </div>
