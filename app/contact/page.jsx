7a64fad29e75ceca9088c889f6ea8b1f569b7ee3
Rajnikantbhay <rsusishere@gmail.com>
2023-10-04 16:58:33 +0530

    new changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -16,6 +16,7 @@ const ContactUs = () => {
   }
   return (
     <>
+<<<<<<< HEAD
     <section className='about-section'>
       <div className='contactSlider'>
         <div>
@@ -40,10 +41,27 @@ const ContactUs = () => {
         <div  className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={emailIcon} width={50} height={50} alt='email' /> <h2>Email</h2> <p>renukiran@info.org</p></div>
 
         </a>
+=======
+    <section className='w-full md:h-[90vh]'>
+  <div className='first-image-container'>
+    <div className='text-container absolute z-20 md:ml-40 ml-6'>
+        <h2 className='uppercase text-white md:font-extrabold md:text-4xl'>Contact Now</h2>
+        <p className='text-slate-400 md:text-sm text-xs'>Donations can provide access to education, healthcare,< br className='md:hidden block'/> and basic necessities, empowering <br className='hidden md:block'/>individuals to break<br className='md:hidden block'/> the cycle of poverty and build a brighter future.</p>
+        <button className='uppercase md:py-3 md:px-8 py-1 px-4 mt-4 text-white hover:bg-[#223752] bg-[#2e5893]'>Call now</button>
+    </div>
+    <img src="https://pixabay.com/get/g479418b809192732a3bd723ee83716eaa38991142ce41c512dd5367cbcd91e546b01606e53b87f831236f205c55ca11b21000a7b8efc5851b9ff197da4c49c37_1280.jpg" alt="image" className='w-full' />
+    
+  </div>
+</section>
+<section>
+      <div className='contact-page-main-content md:flex block justify-center md:my-24 my-14 md:gap-20 md:mx-6'>
+        <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={callingIcon} width={50} height={50} alt='call' /> <h2>Phone</h2> <p>+91 99999999</p></div>
+        <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={emailIcon} width={50} height={50} alt='email' /> <h2>Email</h2> <p>renukiran@info.org</p></div>
+>>>>>>> a341d8d (new changes)
         <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={addressIcon} width={50} height={50} alt='address' /> <h2>Address</h2> <p>temporary location</p></div>
       </div>
     </section>
-    </>
+</>
   )
 }
 
--- app/help/page.jsx
+++ app/help/page.jsx
@@ -26,7 +26,7 @@ const DonateNow = () => {
 
       <div className='md:h-[100vh] h-[60vh] w-full' id='image-container'>
         <div id='img-sub-container' className='w-full h-full grayscale-100'>
-        <div className='absolute z-10 text-center py-3 px-3 donate-div bg-white text-black rounded-lg shadow-md sm:mr-3' style={{opacity:".7"}}>
+        <div className='absolute z-10 text-center py-3 px-3 donate-div bg-white text-black rounded-lg shadow-md sm:mr-3 donation-box' style={{opacity:".7"}}>
           <h3 className='md:text-2xl text-base md:font-bold font-semibold md:mb-4 mb-2'>Give where the need is greatest</h3>
           <p className='md:text-base text-sm md:mb-4 mb-4 text-slate-500'>Your donation makes a big impact! It helps provide education, healthcare, and relief to those less fortunate. Your kindness sparks positive change and brings smiles to faces in need.</p>
           <button className='text-white bg-black md:px-16 md:mb-10 mb-4 px-8 py-2' >DONATE</button>
