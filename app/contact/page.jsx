5afe020cdf7f7fc4ef778a7555113e501d53dfe1
Rajnikantbhay <rsusishere@gmail.com>
2023-10-04 16:58:33 +0530

    new changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -16,34 +16,25 @@ const ContactUs = () => {
   }
   return (
     <>
-    <section className='about-section'>
-      <div className='contactSlider'>
-        <div>
-          <div id='contactSliderDiv'>
-            <div id='firstImageContainer' className='main-image'>
-              <div id='contact-page-overlay'>
-                <div id='contact-now-text'>
-                  <h1 className='ip'>CONTACT NOW</h1>
-                  <hr/>
-                  <p>Contact renukiran now or later as you like <br/>but don't contact other NGOs</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> 
-      </div>
-    </section>
-    <section>
-      <div className='contact-page-main-content md:flex block justify-center md:my-24 my-14 md:gap-20 md:mx-6' >
-        <div  className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4' onClick={makeCall}><Image src={callingIcon} width={50} height={50} alt='call' /> <h2>Phone</h2> <p>+91 9625881835</p></div>
-        <a href="mailto:renukiran@info.org">
-        <div  className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={emailIcon} width={50} height={50} alt='email' /> <h2>Email</h2> <p>renukiran@info.org</p></div>
-
-        </a>
+    <section className='w-full md:h-[90vh]'>
+  <div className='first-image-container'>
+    <div className='text-container absolute z-20 md:ml-40 ml-6'>
+        <h2 className='uppercase text-white md:font-extrabold md:text-4xl'>Contact Now</h2>
+        <p className='text-slate-400 md:text-sm text-xs'>Donations can provide access to education, healthcare,< br className='md:hidden block'/> and basic necessities, empowering <br className='hidden md:block'/>individuals to break<br className='md:hidden block'/> the cycle of poverty and build a brighter future.</p>
+        <button className='uppercase md:py-3 md:px-8 py-1 px-4 mt-4 text-white hover:bg-[#223752] bg-[#2e5893]'>Call now</button>
+    </div>
+    <img src="https://pixabay.com/get/g479418b809192732a3bd723ee83716eaa38991142ce41c512dd5367cbcd91e546b01606e53b87f831236f205c55ca11b21000a7b8efc5851b9ff197da4c49c37_1280.jpg" alt="image" className='w-full' />
+    
+  </div>
+</section>
+<section>
+      <div className='contact-page-main-content md:flex block justify-center md:my-24 my-14 md:gap-20 md:mx-6'>
+        <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={callingIcon} width={50} height={50} alt='call' /> <h2>Phone</h2> <p>+91 99999999</p></div>
+        <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={emailIcon} width={50} height={50} alt='email' /> <h2>Email</h2> <p>renukiran@info.org</p></div>
         <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={addressIcon} width={50} height={50} alt='address' /> <h2>Address</h2> <p>temporary location</p></div>
       </div>
     </section>
-    </>
+</>
   )
 }
 
