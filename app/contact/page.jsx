2bb9839281b35ba9a2591f0375d242823f1bd53c
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-10-05 10:48:21 +0530

    changes as on 5 oct

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import './contact.css'
 import Image from 'next/image'
@@ -6,6 +8,12 @@ import emailIcon from '../../public/assets/icons/email.svg'
 import addressIcon from '../../public/assets/icons/address.svg'
 
 const ContactUs = () => {
+
+
+  function makeCall() {
+    // Replace '1234567890' with the phone number you want to call
+    window.location.href = 'tel:9625881835';
+  }
   return (
     <>
     <section className='about-section'>
@@ -25,9 +33,12 @@ const ContactUs = () => {
       </div>
     </section>
     <section>
-      <div className='contact-page-main-content md:flex block justify-center md:my-24 my-14 md:gap-20 md:mx-6'>
-        <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={callingIcon} width={50} height={50} alt='call' /> <h2>Phone</h2> <p>+91 99999999</p></div>
-        <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={emailIcon} width={50} height={50} alt='email' /> <h2>Email</h2> <p>renukiran@info.org</p></div>
+      <div className='contact-page-main-content md:flex block justify-center md:my-24 my-14 md:gap-20 md:mx-6' >
+        <div  className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4' onClick={makeCall}><Image src={callingIcon} width={50} height={50} alt='call' /> <h2>Phone</h2> <p>+91 9625881835</p></div>
+        <a href="mailto:renukiran@info.org">
+        <div  className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={emailIcon} width={50} height={50} alt='email' /> <h2>Email</h2> <p>renukiran@info.org</p></div>
+
+        </a>
         <div className='md:text-center flex flex-col items-center md:border md:border-solid border-black md:py-8 md:px-12 rounded-lg md:mx-0 mx-4 py-4 my-4'><Image src={addressIcon} width={50} height={50} alt='address' /> <h2>Address</h2> <p>temporary location</p></div>
       </div>
     </section>
---
+++ components/miniCarousel.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function miniCarousel({img1,img2,img3,img4,img5,img6,img7,img8,img9,img10}) {
+  return (
+    <div className='w-[100%] h-[100%]'>
+
+        
+<div id="default-carousel" class="relative w-full" data-carousel="slide">
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img1} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img2} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img3} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img4} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img5} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img6} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img7} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img8} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img9} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img10} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+    </div>
+</div>
+
+      
+    </div>
+  )
+}
+
+export default miniCarousel
