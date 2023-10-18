97457edba8e877ae95980d082c7d28eff072b9c1
Rajnikantbhay <rsusishere@gmail.com>
2023-10-18 15:56:44 +0530

    added about

--- components/teamCards.jsx
+++ components/teamCards.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Image from 'next/image'
 import linkedin from '../public/assets/icons/linkedin.svg'
 import about from '../public/assets/icons/about.svg'
+import './component.css'
 
-function teamCards({name,designation,pic}) {
+function teamCards({name,designation,pic,desc}) {
   return (
     <section class="bg-white dark:bg-gray-900  lg:w-[20vw] w-[70vw] mx-auto h-[50vh] lg:h-[60vh] shadow-md my-8">
-  <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6 about-team">
      
-      <div class="">
+      <div>
           
           <div class="text-center text-gray-500 dark:text-gray-400">
+            
               <img loading='lazy' class="mx-auto mb-4 w-36 h-36 rounded-full" src={pic} alt="Neil Avatar" />
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   <a href="#">{name}</a>
@@ -24,9 +26,12 @@ function teamCards({name,designation,pic}) {
                   </li>
 
                   <li>
-                      <a href="#">
-                          <Image src={about} width={25} height={50}/>
+                    <div>
+                      <a href="#" id='about'>
+                          <Image src={about} width={25} height={50} />
+                          <div className='description'>{desc}</div>
                       </a>
+                      </div>
                   </li>
               </ul>
           </div>
