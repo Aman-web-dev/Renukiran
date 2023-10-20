09613a6a651499e4670bf4439d6311787e48186b
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-10-20 07:28:13 +0530

    Commits

--- app/layout.js
+++ app/layout.js
@@ -29,4 +29,4 @@ export default function RootLayout({ children }) {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script></body>
     </html>
   )
-}
+} 
