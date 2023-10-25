aa299506afd5dd505f0a57f97c4f1566f0651c16
Rajnikantbhay <rsusishere@gmail.com>
2023-10-25 17:13:31 +0530

    changes

--- components/Homeabout.jsx
+++ components/Homeabout.jsx
@@ -4,7 +4,7 @@ const Homeabout = () => {
   return (
     <section className='mb-24 text-black text-normal font-bold w-full text-start'>
         <h3 className='px-8'>
-        "Renukiran is a Civil Society Organisation that promotes programmes through Quality Education, essential Medical facilitation services, Skill trainings and Women empowerment  to underprivileged communities, transforming lives with compassion and dedication. These impactful initiatives have empowered thousands and fostered a brighter future for those in real need across various parts in India."
+        "Renukiran is a Civil Society Organisation that promotes programmes through Quality Education, Digital literacy, Medical facilitation services, Skill trainings and Women empowerment  to underprivileged communities, transforming lives with compassion and dedication. These impactful initiatives have empowered thousands and fostered a brighter future for those in real need across various parts in India."
         </h3>
     </section>
   )
